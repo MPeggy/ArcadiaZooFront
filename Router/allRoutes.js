9645288b111738ab44f5946f64cad5b1a8e6cecf
MPeggy <gypeg426@gmail.com>
2024-09-22 19:29:44 +0200

    Récuperation projet ecf

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,12 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/habitats", "Les habitats", "/pages/habitats.html"),
+    new Route("/services", "Les services", "/pages/services.html"),
+
+];
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia Zoo";
