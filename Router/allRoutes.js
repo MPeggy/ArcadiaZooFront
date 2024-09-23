b3df319249aefdb20133f86a76c833b44da00654
MPeggy <gypeg426@gmail.com>
2024-09-23 18:49:29 +0200

    Interface utilisateur

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,15 +3,17 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
-    new Route("/habitats", "Les habitats", "/pages/habitats/habitats.html", {}),
+    new Route("/habitats", "Les habitats", "/pages/habitats/habitats.html", []),
     new Route("/services", "Les services", "/pages/services.html", []),
     new Route("/savane", "La savane", "/pages/habitats/savane.html",[]),
     new Route("/jungle", "La jungle", "/pages/habitats/jungle.html", []),
     new Route("/marais", "Le marais", "/pages/habitats/marais.html", []),
-    new Route("/signin", "Espace salariés", "/pages/auth/signin.html", ["disconnected"]),
+    new Route("/signin", "Connexion/Déconnexion salariés", "/pages/auth/signin.html", ["disconnected"]),
     new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
-    new Route("/account", "Mon compte", "/pages/auth/account.html"), ["admin", "véto", "employé"],
+    new Route("/account", "Mon compte salarié", "/pages/auth/account.html", ["admin", "véto", "employé"]),
     new Route("/galerie", "Galerie", "/pages/galerie.html", []),
+    new Route("/editPassword", "Changer de mot de passe", "/pages/auth/editPassword.html", []),
+    new Route("/ensavoirplus", "En savoir plus", "/pages/ensavoirplus", []),
 
 ];
 
